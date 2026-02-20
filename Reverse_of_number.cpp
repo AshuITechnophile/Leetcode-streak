87e9ca8c251420a64935244c7ad46d6cdc7bb8eb
Ashu <ashutosh.mishra.royal.max@gmail.com>
2026-02-20 22:25:29 +0530

    Reverse of Number program

--- Reverse_of_number.cpp
+++ Reverse_of_number.cpp
@@ -1,15 +1,24 @@
-class Solution {
-public:
-    int reverse(int x) {
-        int rev = 0;
-        while(x!=0){
-            int k = x%10;
-            if((rev>INT_MAX/10) or (rev<INT_MIN/10)){
-                return 0;
-            } 
-            rev = rev*10+k;
-            x= x/10;
+#include <iostream>
+using namespace std;
+int reverse(int x)
+{
+    int rev = 0;
+    while (x != 0)
+    {
+        int k = x % 10;
+        if ((rev > INT_MAX / 10) or (rev < INT_MIN / 10))
+        {
+            return 0;
         }
-        return rev;
+        rev = rev * 10 + k;
+        x = x / 10;
     }
-};
+    return rev;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout << reverse(n);
+    return 0;
+}
