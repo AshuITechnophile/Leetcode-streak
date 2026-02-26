02ca37478836a52da38c25d626d1785e4e6d7c3b
Ashu <ashutosh.mishra.royal.max@gmail.com>
2026-02-26 22:07:55 +0530

    .\Rotate Array.cpp

---
+++ Rotate Array.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    void rotate(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+        k = k % n;
+        // poora array reverse
+        reverse(nums.begin(), nums.end());
+        // left reverse
+        reverse(nums.begin(), nums.begin() + k);
+        // right reverse
+        reverse(nums.begin() + k, nums.end());
+        return;
+    }
+};
