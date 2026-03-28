33da5370780748b2f307290cd258852afe42ea25
Ashu <ashutosh.mishra.royal.max@gmail.com>
2026-03-28 16:56:39 +0530

    .\134. Gas Station.cpp

---
+++ 134. Gas Station.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost)
+    {
+        int total = 0;
+        int leftgas = 0;
+        int ans = 0;
+
+        for (int i = 0; i < gas.size(); i++)
+        {
+            total += (gas[i] - cost[i]);
+            leftgas += (gas[i] - cost[i]);
+            if (leftgas < 0)
+            {
+                ans = i + 1;
+                leftgas = 0;
+            }
+        }
+        if (total >= 0)
+        {
+            return ans;
+        }
+        return -1;
+    }
+};
