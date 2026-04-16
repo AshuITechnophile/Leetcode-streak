77ab4d4d2239978ddb45ea67d96babb560e3f03f
Ashu <ashutosh.mishra.royal.max@gmail.com>
2026-04-16 09:38:46 +0530

    Put the input field in this

---
+++ 1_Two Sum.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> twoSum(vector<int> &nums, int target)
+{
+    for (int i = 0; i < nums.size(); i++)
+    {
+        for (int j = i + 1; j < nums.size(); j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                return {i, j};
+            }
+        }
+    }
+    return {};
+}
+
+int main()
+{
+    int n;
+    int target;
+    cin >> n;
+    cin >> target;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+    vector<int> result = twoSum(nums, target);
+    if (!result.empty())
+    {
+        cout << result[0] << " " << result[1];
+    }
+    else
+    {
+        cout << "No solution found";
+    }
+    return 0;
+}
