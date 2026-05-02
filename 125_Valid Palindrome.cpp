92a96758d229b2a258f745a889a084144bfa067f
Ashu <ashutosh.mishra.royal.max@gmail.com>
2026-05-02 06:15:20 +0530

    .\125_Valid Palindrome.cpp

---
+++ 125_Valid Palindrome.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+bool isPalindrome(string s)
+{
+    int i = 0;
+    string ans;
+    while (i < s.size())
+    {
+
+        if (s[i] >= 65 && s[i] <= 90)
+        {
+            s[i] += 32;
+        }
+        if (s[i] >= 97 && s[i] <= 122 || s[i] >= 48 && s[i] <= 57)
+        {
+            ans.push_back(s[i]);
+        }
+        i++;
+    }
+    string san;
+    i = ans.size() - 1;
+    while (i >= 0)
+    {
+        san.push_back(ans[i]);
+        i--;
+    }
+    if (san == ans)
+        return true;
+    else
+        return false;
+}
+int main()
+{
+    string s;
+    getline(cin, s);
+    cout << isPalindrome(s);
+    return 0;
+}
