eed8c608055486c317f799415bd41d68b39bcf00
Ashu <ashutosh.mishra.royal.max@gmail.com>
2026-05-14 15:45:55 +0530

    .\1991 Find the Middle Index in Array.cpp

---
+++ 1991 Find the Middle Index in Array.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+ int findMiddleIndex(vector<int> &nums)
+    {
+        int total = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            total = total + nums[i];
+        }
+        int leftsum = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int rightsum = total - leftsum - nums[i];
+            if (leftsum == rightsum)
+            {
+                return i;
+            }
+            leftsum = leftsum + nums[i];
+        }
+        return -1;
+    }
+int main()
+{
+    int n;
+    cout << "Sixe of Array = ";
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+    cout<<findMiddleIndex(nums);
+   return 0;
+}
