39d5d2fd3be6a0bb66a721acd497870e62dc743c
Ashu <ashutosh.mishra.royal.max@gmail.com>
2026-05-23 14:59:32 +0530

    .\3658_GCD of Odd and Even Sums.cpp

---
+++ 3658_GCD of Odd and Even Sums.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int gcdOfOddEvenSums(int n)
+{
+    return n;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout << gcdOfOddEvenSums(n);
+    return 0;
+}
