0dd0965091c17480e977de97ecbe1e520a85ac54
Ashu <ashutosh.mishra.royal.max@gmail.com>
2026-06-04 11:11:59 +0530

    .\274_H-Index.cpp

---
+++ 274_H-Index.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int hIndex(vector<int>& citations) {
+       int n = citations.size();
+       int s = 0;
+       int e = n -1;
+       int ans = 0;
+       sort(citations.begin(),citations.end()); 
+       while(s<=e){
+        int m = s +(e-s)/2;
+        if(citations[m]>=n-m){
+            ans = max(ans,n-m);
+            e = m-1; 
+        }
+        else{
+            s = m+1;
+        }
+       }
+       return ans;
+    }
+int main()
+{
+    int n;
+    cout << "Sixe of Array = ";
+    cin >> n;
+    vector<int> citations(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> citations[i];
+    }
+    cout<<hIndex(citations);
+   return 0;
+}
